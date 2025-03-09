dfdfd30267ca096ed494ce594922687cb11ec6b6
canilearndevops <canilearndevops@gmail.com>
2025-03-09 12:19:02 +0530

    added @component and @componentscan annotation

--- src/main/java/com/example/beans/Vehicle.java
+++ src/main/java/com/example/beans/Vehicle.java
@@ -1,5 +1,9 @@
 package com.example.beans;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class Vehicle {
     private String name;
 
@@ -9,5 +13,9 @@ public class Vehicle {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("printing hello from the vehicle class");
     }
 }
--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -2,47 +2,24 @@ package com.example.config;
 
 import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+
 /*
+Spring @Configuration annotation is part of the spring core framework.
+Spring Configuration annotation indicates that the class has @Bean definition
+methods. So Spring container can process the class and generate Spring Beans
+to be used in the application.
 
-   Primary annotation is used when if we don't mention the name of the bean we still can refer the
-   bean annotated with primary annotation.
-
-    When you have multiple beans of the same kind inside the Spring context,
-    you can make one of them primary by using @Primary annotation. Primary bean is
-    the one which Spring will choose if it has multiple options and you don’t specify a name.
-    In other words, it is the default bean that Spring Context will consider in case of
-    confusion due to multiple beans present of same type.
-
-*/
-
+To tell Spring it needs to search for classes annotated
+with stereotype annotations, we use the @ComponentScan annotation over the configuration
+class.
+* */
 
 @Configuration
+@ComponentScan(basePackages="com.example.beans")
 public class ProjectConfig {
-
-    @Bean(name="audiVehicle")
-    @Primary
-    Vehicle vehicle1(){
-        var veh = new Vehicle();
-        veh.setName("Audi");
-        return veh;
-    }
-
-    @Bean(value="Honda")
-    Vehicle vehicle2(){
-        var veh = new Vehicle();
-        veh.setName("Honda");
-        return veh;
-    }
-
-    @Bean("ferrariVehicle")
-    Vehicle vehicle3(){
-        var veh = new Vehicle();
-        veh.setName("Toyota");
-        return veh;
-    }
-
 
 }
